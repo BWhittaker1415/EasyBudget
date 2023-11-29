8c05a5ebb9cd3c2298d10d4fe293d7275b020320
BWhittaker1415 <bwhittaker1415@gmail.com>
2023-11-28 22:22:25 -0500

    More Redux, Installed Toastify

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,1 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { build } from "vite";
+
+// baseUrl is an empty '' because I have a proxy already set up in "vite.config.js"
+const baseQuery = fetchBaseQuery({ baseUrl: "" });
+
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ["User"],
+  endpoints: (builder) => ({}),
+});

3b78f9dd1c7201f7025032267267088f948c169c
BWhittaker1415 <bwhittaker1415@gmail.com>
2023-12-01 20:51:12 -0500

    accountModel updated

---
+++ backend/models/accountModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    bank: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    creditCard: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Account = mongoose.model("Account", accountSchema);
+
+export default Account;
